b0136f41d1653d6c7b89a3cdf850ed54fe5a6a13
Alex Lew <a.lew@yale.edu>
2019-05-01 15:04:53 -0400

    added offset angle for spin and capture

--- js/sentinel.js
+++ js/sentinel.js
@@ -6,7 +6,7 @@ function sentinel() {
 // CAREFUL --- img_warehouse is a global variable for storing blobs; only reference in capture()
 var img_warehouse = [];
 
-function capture( sketchfab_api, img_type = 'image/png', initial_camera, frame_index, total_frames = 1047, width = 1920, height = 1080 ) {
+function capture( sketchfab_api, img_type = 'image/png', initial_camera, frame_index, total_frames = 1047, width = 1920, height = 1080, offset = 0.0 ) {
 
     // base case: zip all img blobs stored in img_warehouse[] and save to .ZIP file
     if ( frame_index === total_frames ) {
@@ -43,7 +43,7 @@ function capture( sketchfab_api, img_type = 'image/png', initial_camera, frame_i
         return;
     }
 
-    let camera_promise = turntable( initial_camera, frame_index, total_frames );
+    let camera_promise = turntable( initial_camera, frame_index, total_frames, offset );
     camera_promise.then( function( new_camera ) {
         lookat_wrapper( sketchfab_api, new_camera.position, new_camera.target, 0.0 );
         console.log( 'called lookat at frame ' + frame_index );
@@ -54,7 +54,7 @@ function capture( sketchfab_api, img_type = 'image/png', initial_camera, frame_i
         img_warehouse.push( img_blob );
         console.log( 'called push at frame ' + frame_index );
     }).then( function() {
-        capture( sketchfab_api, img_type, initial_camera, frame_index + 1, total_frames, width, height );
+        capture( sketchfab_api, img_type, initial_camera, frame_index + 1, total_frames, width, height, offset );
     });
 }
 
@@ -84,15 +84,15 @@ function lookat_wrapper( sketchfab_api, eye, target, duration ) {
 }
 
 // turntable returns a camera object for a desired position around the model
-function turntable( initial_camera, frame_index, total_frames ) {
+function turntable( initial_camera, frame_index, total_frames, offset = 0.0 ) {
     return new Promise(( resolve, reject ) => {
         var increment = ( 2 * Math.PI ) / total_frames;
         var angle = increment * ( frame_index + 1 );
         var distance = Math.sqrt(
             Math.pow( initial_camera.target[ 0 ] - initial_camera.position[ 0 ], 2 ) +
             Math.pow( initial_camera.target[ 1 ] - initial_camera.position[ 1 ], 2 ));
-        var x = initial_camera.target[ 0 ] + distance * Math.cos( angle );
-        var y = initial_camera.target[ 1 ] + distance * Math.sin( angle );
+        var x = initial_camera.target[ 0 ] + distance * Math.cos( angle + offset );
+        var y = initial_camera.target[ 1 ] + distance * Math.sin( angle + offset );
 
         var new_camera = {
             position: [ x, y, initial_camera.position[ 2 ]],
